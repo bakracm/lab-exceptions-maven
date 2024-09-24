a8b20b9a52d0f0a68264d2de4dcffac6b5b62e80
bakracmi <bakracm@gmail.com>
2024-09-24 15:45:56 -0500

    Attemped to implement smallerRoot()

--- src/main/java/edu/grinnell/csc207/util/Quadratic.java
+++ src/main/java/edu/grinnell/csc207/util/Quadratic.java
@@ -1,5 +1,7 @@
 package edu.grinnell.csc207.util;
 
+import java.lang.Math;
+
 /**
  * Quadratic expressions (of the form ax^2 + bx + c).
  *
@@ -95,7 +97,7 @@ public class Quadratic {
    *   is only one).
    */
   public double smallerRoot() {
-    return 0.0; // STUB
+    return ((-1) * this.b - (java.lang.Math.sqrt( this.b * this.b - (4 * this.a * this.c))));
   } // smallerRoot
 
   /**
